955c6591a935e9e92b0692a167533d9f3a574083
wongcommand <versyario@gmail.com>
2017-02-16 06:44:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ module.exports = {
 	// If you replace YOUR_BOT_USER_ID with your bot's user ID, the prefix will be a mention to
 	// your bot. You can get that ID in https://discordapp.com/developers/applications/me/
 	// (click on your application, and find it under "App Details" > "Client ID"
-	prefix:  "<@CCBOT#0864>",
+	prefix:  "<@281087449139249152>",
 
 	// This is a readable version of the prefix. Generally, this is the same as prefix, but if
 	// you set prefix to be in the form of "<@YOUR_BOT_USER_ID>", you'd need to set readable_prefix
